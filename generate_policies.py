773528a818bc64d993e49571b5e10a4c7e1edaf0
t-rugg <70915141+t-rugg@users.noreply.github.com>
2023-12-16 15:06:39 -0700

    beginning work on policies... ughhhh

---
+++ generate_policies.py
@@ -0,0 +1,18 @@
+# BANNED MODIFIERS: dev cost, core cost, diplo annex cost, anything related to gov cap, province warscore cost
+# players shouldnt be "forced" to take a specific 2-idea combo if their playstyle revolves around these modifiers
+
+adm = {
+    "innovativeness_ideas": ("advisor_cost = -0.1", "trade_efficiency = 0.1", "infantry_power = 0.075"),
+    "religious_ideas": ("missionary_strength = 0.015", "tolerance_own = 1", "morale_damage = 0.075"),
+    "economic_ideas": ("inflation_reduction = 0.1", "production_efficiency = 0.1", "land_forcelimit_modifier = 0.1"),
+    "expansion_ideas": ("build_cost = -0.1", "global_colonial_growth = 15" , "global_manpower_modifier = 0.1"),
+    "administrative_ideas": ("overextension_impact_modifier = -0.05", "adm_advisor_cost = -0.2", "drill_decay_modifier = -0.25"),
+    "humanist_ideas": ("idea_cost = -0.075", "improve_relations = 0.15", "war_exhaustion = -0.03"),
+    "infrastructure_ideas": ("build_time = -0.1", "global_trade_power = 0.1", "movement_speed = 0.075")
+}
+dip = {
+
+}
+mil = {
+    
+}
