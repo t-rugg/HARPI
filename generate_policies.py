5efe7210ff48dc14522319f4fa6ffa52407fb118
t-rugg <70915141+t-rugg@users.noreply.github.com>
2023-12-16 23:26:32 -0700

    naval ideas + all admin group policy modifiers in

--- generate_policies.py
+++ generate_policies.py
@@ -1,14 +1,20 @@
-# BANNED MODIFIERS: dev cost, core cost, diplo annex cost, anything related to gov cap, province warscore cost
+# BANNED MODIFIERS: dev cost, core cost, diplo annex cost, admin eff, anything related to gov cap, province warscore cost
 # players shouldnt be "forced" to take a specific 2-idea combo if their playstyle revolves around these modifiers
 
 adm = {
     "innovativeness_ideas": ("advisor_cost = -0.1", "trade_efficiency = 0.1", "infantry_power = 0.075"),
     "religious_ideas": ("missionary_strength = 0.015", "tolerance_own = 1", "morale_damage = 0.075"),
-    "economic_ideas": ("inflation_reduction = 0.1", "production_efficiency = 0.1", "land_forcelimit_modifier = 0.1"),
+    "economic_ideas": ("inflation_reduction = 0.1", "build_time = -0.1", "land_forcelimit_modifier = 0.1"),
     "expansion_ideas": ("build_cost = -0.1", "global_colonial_growth = 15" , "global_manpower_modifier = 0.1"),
     "administrative_ideas": ("overextension_impact_modifier = -0.05", "adm_advisor_cost = -0.2", "drill_decay_modifier = -0.25"),
     "humanist_ideas": ("idea_cost = -0.075", "improve_relations = 0.15", "war_exhaustion = -0.03"),
-    "infrastructure_ideas": ("build_time = -0.1", "global_trade_power = 0.1", "movement_speed = 0.075")
+    "infrastructure_ideas": ("build_time = -0.1", "global_trade_power = 0.1", "movement_speed = 0.075"),
+    "extraction_ideas": ("global_trade_goods_size_modifier = 0.075",  "production_efficiency = 0.1", "mercenary_cost = -0.15"),
+    "urbanization_ideas": ("global_prosperity_growth = 0.25", "num_accepted_cultures = 1", "mercenary_manpower = 0.2"),
+    "militarist_ideas": ("war_exhaustion = -0.03", "reinforce_cost_modifier = -0.15", "leader_cost = -0.15"),
+    "federalist_ideas": ("state_maintenance_modifier = -0.25", "diplomatic_upkeep = 1", "discipline = 0.05"),
+    "governance_ideas": ("global_tax_modifier = 0.15", , "reserves_organisation = 0.1"),
+    "metallurgy_ideas": ("prestige = 1", "caravan_power = 0.25", "fire_damage = 0.075")
 }
 dip = {
 
