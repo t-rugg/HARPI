593cf76f502a53f828e489c4cd9169540ece6ff3
t-rugg <70915141+t-rugg@users.noreply.github.com>
2023-12-18 12:53:32 -0700

    policies and legacies

--- generate_policies.py
+++ generate_policies.py
@@ -1,4 +1,4 @@
-# BANNED MODIFIERS: dev cost, core cost, diplo annex cost, admin eff, anything related to gov cap, province warscore cost, innovativeness gain, idea cost, tech cost, accept vassal reasons
+# BANNED MODIFIERS: dev cost, core cost, diplo annex cost, admin eff, anything related to gov cap, province warscore cost, innovativeness gain, idea cost, tech cost, accept vassal reasons, num/cost accepted cultures
 # players shouldnt be "forced" to take a specific 2-idea combo if their playstyle revolves around these modifiers
 # or to turn on/off policies as they tech up/take ideas
 #
@@ -23,14 +23,14 @@ random.seed("HARPI")
 
 adm = {
 	"innovativeness_ideas": ("advisor_cost = -0.1", "trade_efficiency = 0.1", "infantry_power = 0.075", "global_institution_spread = 0.25", "reform_progress_growth = 0.15", "artillery_fire = 0.02"),
-	"religious_ideas": ("missionary_strength = 0.015", "tolerance_own = 1", "morale_damage = 0.075", "max_absolutism = 5", "reverse_relation_with_same_religion = 10", "fire_damage_received = -0.075"),
+	"religious_ideas": ("missionary_strength = 0.015", "tolerance_own = 1.5", "morale_damage = 0.075", "max_absolutism = 5", "reverse_relation_with_same_religion = 10", "fire_damage_received = -0.075"),
 	"economic_ideas": ("inflation_reduction = 0.1", "build_time = -0.1", "land_forcelimit_modifier = 0.15", "global_tax_modifier = 0.15", "heavy_ship_power = 0.1", "drill_decay_modifier = -0.25"),
 	"expansion_ideas": ("build_cost = -0.1", "global_colonial_growth = 10" , "global_manpower_modifier = 0.15", "religious_unity = 0.1", "trade_steering = 0.25", "merc_leader_army_tradition = 0.25"), # todo
 	"administrative_ideas": ("overextension_impact_modifier = -0.05", "adm_advisor_cost = -0.2", "yearly_army_professionalism = 0.002", "global_autonomy = -0.075", "yearly_corruption = -0.1", "mercenary_discipline = 0.05"),
-	"humanist_ideas": ("global_unrest  = -1", "improve_relations = 0.15", "land_leader_maneuver = 1", "stability_cost_modifier = -0.15", "sailors_recovery_speed = 0.15", "morale_damage_received = -0.075"),
+	"humanist_ideas": ("global_unrest  = -1", "improve_relations = 0.15", "land_leader_maneuver = 1", "stability_cost_modifier = -0.15", "global_sailors_modifier = 0.2", "morale_damage_received = -0.075"),
 	"infrastructure_ideas": ("build_time = -0.1", "global_trade_power = 0.1", "movement_speed = 0.075", "interest = -0.05", "envoy_travel_time = -0.33", "reinforce_speed = 0.15"),
 	"extraction_ideas": ("global_trade_goods_size_modifier = 0.075",  "production_efficiency = 0.1", "mercenary_cost = -0.15", "loot_amount = 0.5", "vassal_income = 0.25", "siege_ability = 0.15"),
-	"urbanization_ideas": ("global_prosperity_growth = 0.25", "num_accepted_cultures = 1", "mercenary_manpower = 0.25", "global_autonomy = -0.075", "estate_interaction_cooldown_modifier = -0.20", "land_leader_fire = 1"),
+	"urbanization_ideas": ("global_prosperity_growth = 0.25", "naval_forcelimit_modifier = 0.15", "mercenary_manpower = 0.25", "global_autonomy = -0.075", "estate_interaction_cooldown_modifier = -0.20", "land_leader_fire = 1"),
 	"militarist_ideas": ("war_exhaustion = -0.03", "reinforce_cost_modifier = -0.15", "leader_cost = -0.15", "global_unrest = -1", "mil_advisor_cost = -0.2", "army_tradition_decay = -0.01"),
 	"federalist_ideas": ("state_maintenance_modifier = -0.25", "diplomatic_reputation = 1", "discipline = 0.035", "state_maintenance_modifier = -0.25", "reduced_liberty_desire_on_same_continent = 10", "defensiveness = 0.15"),
 	"governance_ideas": ("global_tax_modifier = 0.15", "global_own_trade_power = 0.15", "reserves_organisation = 0.1", "years_of_nationalism = -5", "diplomatic_upkeep = 1", "reinforce_speed = 0.15"),
@@ -39,8 +39,8 @@ adm = {
 }
 dip = {
 	"spy_ideas": ("max_absolutism = 5", "dip_advisor_cost = -0.2", "leader_siege = 1", "interest = -0.05", "global_foreign_trade_power = 0.15", "harsh_treatment_cost = -0.25"),
-	"diplomatic_ideas": (),
-	"trade_ideas": (),
+	"diplomatic_ideas": ("prestige = 1", "diplomatic_upkeep = 1", "yearly_army_professionalism = 0.002", "tolerance_heathen = 1.5", "reduced_trade_penalty_on_non_main_tradenode = 0.25", "manpower_in_accepted_culture_provinces = 0.2"),
+	"trade_ideas": ("global_institution_spread = 0.25", "trade_company_investment_cost = -0.1", "discipline = 0.035", "reform_progress_growth = 0.15", "leader_naval_manuever = 1", "mercenary_discipline = 0.05"),
 	"exploration_ideas": (),
 	"maritime_ideas": (),
 	"court_ideas": (),
@@ -51,7 +51,8 @@ dip = {
 	"authority_ideas": (),
 }
 mil = {
-	"offensive_ideas": ("loot_amount = 0.5", "improve_relations = 0.15", "shock_damage = 0.075", "years_of_nationalism = -5", "trade_company_investment_cost = -0.1", "artillery_barrage_cost = -0.25"),
+	"offensive_ideas": ("loot_amount = 0.5", "heavy_ship_cost = -0.1", "shock_damage = 0.075", "years_of_nationalism = -5", "trade_company_investment_cost = -0.1", "artillery_barrage_cost = -0.25"),
+	"defensive_ideas": ("build_time = -0.1", "improve_relations = 0.15", "shock_damage_received = -0.075", "global_prosperity_growth = 0.25", "disengagement_chance = 0.1", "manpower_in_own_culture_provinces = 0.2")
 }
 
 combined = adm | dip | mil
@@ -115,7 +116,9 @@ with open("generated_policies.txt", "w", encoding="windows-1252") as f:
 	for group1 in ideas:
 		cat1 = getCategory(group1)
 		blacklist.append(group1)
-		for group2 in [key for key in ideas if key not in blacklist]:
+		for group2 in ideas:
+			if group2 in blacklist:
+				continue
 			cat2 = getCategory(group2)
 			if (cat1 != cat2):
 				# get a random modifier from the appropriate category
