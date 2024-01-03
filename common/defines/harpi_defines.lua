42e99ebc03413ce8d16434531d61e3d511c529d9
t-rugg <70915141+t-rugg@users.noreply.github.com>
2024-01-02 18:39:19 -0700

    consistent whitespace

--- common/defines/harpi_defines.lua
+++ common/defines/harpi_defines.lua
@@ -4,9 +4,9 @@
 
 -- HARPI values
 NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.35 -- from 1-1-0 to 1-1-1
-NDefines.NCountry.FREE_IDEA_GROUP_COST  = 4	-- from 3
-NDefines.NCountry.IDEA_TO_TECH = -0.01	-- from -2%. twice as many groups means we halve the tech cost reduction
-                                        -- this is probably a safe assumption since the reduction is only really significant lategame
+NDefines.NCountry.FREE_IDEA_GROUP_COST = 4 -- from 3
+NDefines.NCountry.IDEA_TO_TECH = -0.01 -- from -2%. twice as many groups means we halve the tech cost reduction
+                                       -- this is probably a safe assumption since the reduction is only really significant lategame
 NDefines.NCountry.PS_BUY_IDEA = 300 -- from 400. idea groups are meant to be about ~75% as powerful as a "strong group" in vanilla, give or take
 NDefines.NCountry.INNOVATIVENESS_FIRST_PICKED_IDEA = 0.5 -- from 2 (low because there are more idea groups, i.e. much easier to get a unique one)
 
